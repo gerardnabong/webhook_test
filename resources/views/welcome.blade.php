9a3d93b3c68abca498ade066d0e76ab000fa3119
gerardnabong <gerard.nabong@arbcalls.com>
2022-02-02 22:47:51 +0800

    test1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test One
+                    Laravel Test Two
                 </div>
 
                 <div class="links">
